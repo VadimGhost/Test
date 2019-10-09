ec3ff833055844f0b4f9b5f79ee840d0a45dd598
VadimGhost <wasdgr@gmail.com>
2019-10-09 22:43:01 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,9 @@ public class Main
     public static void main(String[] args)
     {
         int K = 0;
-        File file = new File("C:\\Users\\Zver\\Desktop\\Input_file.txt");
+        double MaxSquare = 0;
 
+        File file = new File("C:\\Users\\Zver\\Desktop\\Input_file.txt");
         try
         {
 
@@ -19,54 +20,56 @@ public class Main
             DataInputStream InBuf = new DataInputStream(StringFile);
             BufferedReader br = new BufferedReader(new InputStreamReader(InBuf));
             String strLine;
-
             while ((strLine = br.readLine()) != null)
-            {
-
-              K++;
-              System.out.println(strLine);
-              String arr = strLine;
-              String[] CoordsStr = arr.split(" ");
-              int[] Coords = new int[CoordsStr.length];
-                for (int i = 0; i < CoordsStr.length; i++)
                 {
-                    try {
-                        Coords[i] = Integer.parseInt(CoordsStr[i]);
-                        }
-                    catch (NumberFormatException nfe)
-                    {  System.err.println("Ошибка в написании координат на линии"+K);}
-                }
 
-              if(Coords.length == 6)
-                {
-                    Double Line1 = SizeLength1(Coords);
-                    Double Line2 = SizeLength2(Coords);
-                    Double Line3 = SizeLength3(Coords);
-
-
-
-
-
-                    if ((Line1.equals(Line2))||(Line2.equals(Line3))||(Line3.equals(Line1)))
+                  K++;                                                                                                      // считаем строку по файлу
+                  System.out.println(strLine);
+                  String arr = strLine;
+                  String[] CoordsStr = arr.split(" ");                                                                 // разделение строки по пробелу
+                  int[] Coords = new int[CoordsStr.length];
+                  for (int i = 0; i < CoordsStr.length; i++)                                                               //парсим строку в числа
                     {
-                        Double s1 = TriangleSqure(Line1, Line2, Line3);
-                        System.out.println(s1);
-                    }
-                    else
-                    {
-                        System.out.println("Треугольник не является равноберенным на строке:"+K);
+                        try {
+                             Coords[i] = Integer.parseInt(CoordsStr[i]);
+                            }
+                        catch (NumberFormatException nfe)
+                            {
+                             System.err.println("Ошибка в написании координат на линии"+K);
+                            }
                     }
 
-                }
-              else
-                {
-                    System.out.println(Coords[5]);
-                    System.out.println();
-                }
+                  if(Coords.length == 6)
+                     {
+                        Double Line1 = SizeLength1(Coords);
+                        Double Line2 = SizeLength2(Coords);
+                        Double Line3 = SizeLength3(Coords);
+
+                        if ((Line1.equals(Line2))||(Line2.equals(Line3))||(Line3.equals(Line1)))
+                            {
+                            double s1 = TriangleSqure(Line1, Line2, Line3);
+                            if (s1>MaxSquare)
+                            {
+
+                            }
+
+                            System.out.println(s1);
+
+                            }
+                        else
+                            {
+                            System.out.println("Треугольник не является равноберенным на строке:"+K);
+                             }
+                     }
+                  else
+                    {
+                        System.out.println(Coords[5]);
+                        System.out.println();
+                    }
 
 
-                System.out.println(Coords[1]);
-            }
+                    System.out.println(Coords[1]);
+                }
             InBuf.close();
         }
 
@@ -82,30 +85,25 @@ public class Main
     {
         double x = a[0]-a[2];
         double y = a[1]-a[3];
-        double LengthSide = sqrt(x*x+y*y);
-
-        return LengthSide;
+        return sqrt(x*x+y*y);
     }
     private static double SizeLength2 (int[] a)
     {
         double x = a[2]-a[4];
         double y = a[3]-a[5];
-        double LengthSide = sqrt(x*x+y*y);
-        return LengthSide;
+        return sqrt(x*x+y*y);
     }
     private static double SizeLength3 (int[] a)
     {
         double x = a[4]-a[0];
         double y = a[5]-a[1];
-        double LengthSide = sqrt(x*x+y*y);
-        return LengthSide;
+        return sqrt(x*x+y*y);
     }
 
     private static double TriangleSqure (double Length1, double Length2, double Length3)
     {
-        Double Perimeter = Length1+Length2+Length3;
-        Double Square = sqrt(Perimeter*(Perimeter-Length1)*(Perimeter-Length2)*(Perimeter-Length3));
-        return Square;
+        double Perimeter = Length1+Length2+Length3;
+        return sqrt(Perimeter*(Perimeter-Length1)*(Perimeter-Length2)*(Perimeter-Length3));
     }
 
 }
